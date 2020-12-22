5c4ac1f8bb9ada6a43333ea99a77161250f3a81a
Bastien <bastien.fontaine18520@gmail.com>
2020-12-22 17:12:26 +0100

    Market défini comme class
    Faut implémenter un def handler dans le market et une création de process economics et politics

--- Projet.py
+++ Projet.py
@@ -10,13 +10,13 @@ import time
 import random
 
 
-def handler(sig, frame):
-    for i in range(len(signaux)):
-        if sig == signaux[i] :
-            pass
+#def handler(sig, frame):
+#    for i in range(len(signaux)):
+#        if sig == signaux[i] :
+#            pass
 
-for i in range(len(signaux)):
-    signal.signal(signaux[i],handler)
+#for i in range(len(signaux)):
+#    signal.signal(signaux[i],handler)
 
 dico_signaux = {}
 
@@ -57,35 +57,40 @@ def weather(proba_disaster_H,proba_disaster_S,shared_memory):
     
     print("le process s'est éxécuté")
 
-def market (shared_memory,coeff):
-    energy_Price = 0.5
-    energy_In = 0.0
-    energy_Out = 0.0
-    long_term_coeff = 0.999
-    max_thread = 3
+class market(ms.Process):
+    def __init__ (self, shared_memory, coeff):
+        super().__init__()
+        self.coeff = coeff
+        self.shared_memory = shared_memory
+        self.energy_Price = 0.5
+        self.energy_In = 0.0
+        self.energy_Out = 0.0
+        self.long_term_coeff = 0.999
+        self.max_thread = 3
+        self.nb_thread = 0
 
-    start = time.time()
-    c=0
-    while c<=5:
-        end = time.time()
+    def run(self):
 
+        start = time.time()
+        c=0
+        while c<=5:
+            end = time.time()
 
-        if (end-start >= 5) :
-            nb_thread = 0
-            temperature = float("{:.2f}".format(shared_memory[1]))
-            natural_disast_s = float("{:.2f}".format(shared_memory[2]))
-            natural_disast_h = float("{:.2f}".format(shared_memory[3]))
-            temp2 = float("{:.2f}".format(1/temperature))
+            if (end-start >= 5) :
+                self.nb_thread = 0
+                temperature = float("{:.2f}".format(self.shared_memory[1]))
+                natural_disast_s = float("{:.2f}".format(self.shared_memory[2]))
+                natural_disast_h = float("{:.2f}".format(self.shared_memory[3]))
 
-            eco_pol = ms.Process(target=economics_politics, args=())
-            eco_pol.start()
-            eco_pol.join()
-            eco_pol.terminate()
+                #eco_pol = ms.Process(target=economics_politics, args=())
+                #eco_pol.start()
+                #eco_pol.join()
+                #eco_pol.terminate()
 
-            energy_Price = energy_Price*long_term_coeff + (1/temperature)*coeff[0] + natural_disast_s*coeff[1] + natural_disast_h*coeff[2]  
-            print("energy price : ", energy_Price)
-            start = time.time() 
-            c+=1 
+                self.energy_Price = self.energy_Price*self.long_term_coeff + (1/temperature)*self.coeff[0] + natural_disast_s*self.coeff[1] + natural_disast_h*self.coeff[2]  
+                print("energy price : ", self.energy_Price)
+                start = time.time() 
+                c+=1 
 
 def economics_politics():
     proba = [10^(-15),10^(-8),10^(-4),10^(-6),10^(-3),10^(-4)]
@@ -107,7 +112,7 @@ if __name__ == "__main__":
     coeff = [0.003,0.002,0.01,0.02,0.006,0.008,0.004,0.001,0.001]
 
     weather_process = ms.Process(target=weather, args=(proba_disaster_H,proba_disaster_S,shared_memory))
-    market_process = ms.Process(target=market, args=(shared_memory,coeff))
+    market_process = market(shared_memory, coeff)
 
     weather_process.start()
     print("le process weather démarre")
